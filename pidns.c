2a75ce2c61a52af5eb003689783ce48e5991efd1
Badri Janakiraman <badri.j@gmail.com>
2016-03-17 09:04:58 -0700

    Add an example for pid ns

---
+++ pidns.c
@@ -0,0 +1,29 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#define STACK_SIZE (1024 * 1024)
+static char child_stack[STACK_SIZE];    /* Space for child's stack */
+
+static int childFunc() {
+  printf("childFunc(): PID  = %ld\n", (long) getpid());
+  printf("childFunc(): PPID = %ld\n", (long) getppid());
+  while(true){}
+}
+
+int main(int argc, char *argv[]) {
+    printf("main(): PID  = %ld\n", (long) getpid());
+    printf("main(): PPID  = %ld\n", (long) getppid());
+
+    char *stack = malloc(STACK_SIZE);
+    char *stackTop = stack + STACK_SIZE;  /* Linux memory addresss grow downwards */
+
+    pid_t child_pid = clone(childFunc,
+                            stackTop,         /* Stack space for child process */
+                            CLONE_NEWPID | SIGCHLD, NULL); /*Allows pid namespaceing & for child to signal death to parent*/
+    waitpid(child_pid, NULL, 0);
+}
